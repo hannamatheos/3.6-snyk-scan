97a3ed6425078799ef33e29beebe5002307936b5
hannamatheos <hannamatheos@gmail.com>
2025-07-24 21:43:37 +0800

    remove branch protection

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "owner_name" {
   description = "Name to distinguish resources"
   type        = string
-  default     = "hanna"  # or leave out the default to force input
+  default     = "hanna"
 }
